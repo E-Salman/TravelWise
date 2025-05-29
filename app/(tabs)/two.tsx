7e8b4905f2aa5a855fef1df2adc0b3441d4e9fbb
E-Salman <esalman@hotmail.com>
2025-05-29 18:22:03 -0300

    changed absolute path to relative for TWlogo.png

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -10,7 +10,7 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Image
-        source={require('C:/Users/Admin/TravelWise/assets/images/TWlogo.png')} // adjust path as needed
+        source={require('../../assets/images/TWlogo.png')} // adjust path as needed
         style={{ width: 200, height: 200 }}
       />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
